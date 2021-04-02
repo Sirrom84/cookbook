f6eb86c80d3895a904e30e71d1e106d28b225770
Rob  Morris <morrisrjc@gmail.com>
2021-04-02 14:20:57 -0600

    getting started messing around

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,46 @@
+import React, {useState} from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import {Link} from "react-router-dom";
+import {MenuData} from "./MenuData";
+import "./Menu.css";
+import {IconContext} from "react-icons";
+
+function Menu() {
+	const [menu, setMenu] = useState(false);
+
+	const showMenu = () => setMenu(!menu);
+
+	return (
+		<>
+			{/* <IconContext.Provider value={{color: "#fff"}}> */}
+			<div className="navbar">
+				<Link to="#" className="menu-bars">
+					<FaIcons.FaBars onClick={showMenu} />
+				</Link>
+			</div>
+			<nav className={menu ? "nav-menu active" : "nav-menu"}>
+				<ul className="nav-menu-items" onClick={showMenu}>
+					<li className="navbar-toggle">
+						<Link to="#" className="menu-bars">
+							<AiIcons.AiOutlineClose />
+						</Link>
+					</li>
+					{MenuData.map((item, index) => {
+						return (
+							<li key={index} className={item.cName}>
+								<Link to={item.path}>
+									{item.icon}
+									<span>{item.title}</span>
+								</Link>
+							</li>
+						);
+					})}
+				</ul>
+			</nav>
+			{/* </IconContext.Provider> */}
+		</>
+	);
+}
+
+export default Menu;
